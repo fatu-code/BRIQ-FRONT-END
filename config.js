21488f9d21df6e14a89a32d586e8fdd863b80b89
fatu-code <fatumeshariff@gmail.com>
2026-06-22 23:42:19 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Change API_BASE to your deployed backend URL (e.g. https://brik-api.up.railway.app)
 window.BRIK_CONFIG = {
-  API_BASE: "http://localhost:4000",
+  API_BASE: "https://briq-back-end-production.up.railway.app",
 };
